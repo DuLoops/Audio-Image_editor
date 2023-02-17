971d88d14f4e73e2fb427948e8e0a12402b7efcf
Dujin <kdj00718@gmail.com>
2023-02-16 18:44:41 -0800

    Initial upload

---
+++ Audio-Editor/WindowsFormsApp/Form2.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dftChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nVal = new System.Windows.Forms.NumericUpDown();
+            this.lowfilter = new System.Windows.Forms.RadioButton();
+            this.highfilter = new System.Windows.Forms.RadioButton();
+            this.filter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dftChart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dftChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.dftChart.ChartAreas.Add(chartArea1);
+            this.dftChart.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dftChart.Location = new System.Drawing.Point(11, 115);
+            this.dftChart.Name = "dftChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.dftChart.Series.Add(series1);
+            this.dftChart.Size = new System.Drawing.Size(741, 367);
+            this.dftChart.TabIndex = 0;
+            this.dftChart.Text = "chart1";
+            this.dftChart.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(539, 34);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(160, 50);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save/IDFT";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nVal);
+            this.groupBox1.Controls.Add(this.lowfilter);
+            this.groupBox1.Controls.Add(this.highfilter);
+            this.groupBox1.Controls.Add(this.filter);
+            this.groupBox1.Location = new System.Drawing.Point(59, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(248, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // nVal
+            // 
+            this.nVal.Location = new System.Drawing.Point(147, 20);
+            this.nVal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nVal.Name = "nVal";
+            this.nVal.Size = new System.Drawing.Size(96, 22);
+            this.nVal.TabIndex = 3;
+            this.nVal.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lowfilter
+            // 
+            this.lowfilter.AutoSize = true;
+            this.lowfilter.Location = new System.Drawing.Point(18, 32);
+            this.lowfilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lowfilter.Name = "lowfilter";
+            this.lowfilter.Size = new System.Drawing.Size(98, 20);
+            this.lowfilter.TabIndex = 2;
+            this.lowfilter.TabStop = true;
+            this.lowfilter.Text = "Low - Pass";
+            this.lowfilter.UseVisualStyleBackColor = true;
+            this.lowfilter.CheckedChanged += new System.EventHandler(this.lowfilter_CheckedChanged);
+            // 
+            // highfilter
+            // 
+            this.highfilter.AutoSize = true;
+            this.highfilter.Location = new System.Drawing.Point(18, 66);
+            this.highfilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.highfilter.Name = "highfilter";
+            this.highfilter.Size = new System.Drawing.Size(102, 20);
+            this.highfilter.TabIndex = 3;
+            this.highfilter.TabStop = true;
+            this.highfilter.Text = "High - Pass";
+            this.highfilter.UseVisualStyleBackColor = true;
+            // 
+            // filter
+            // 
+            this.filter.Location = new System.Drawing.Point(147, 50);
+            this.filter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.filter.Name = "filter";
+            this.filter.Size = new System.Drawing.Size(96, 46);
+            this.filter.TabIndex = 0;
+            this.filter.Text = "Filter";
+            this.filter.UseVisualStyleBackColor = true;
+            this.filter.Click += new System.EventHandler(this.filter_click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 493);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.dftChart);
+            this.Name = "Form2";
+            this.Text = "DFT";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dftChart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nVal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart dftChart;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button filter;
+        private System.Windows.Forms.RadioButton highfilter;
+        private System.Windows.Forms.RadioButton lowfilter;
+        private System.Windows.Forms.NumericUpDown nVal;
+    }
+}
---
+++ Audio-Editor/WindowsFormsApp/Form2.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;//needed to perform P/Invoke - win32 calls
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+using System.Threading;
+using static WindowsFormsApp.WaveHeader;
+
+namespace WindowsFormsApp
+{
+    public partial class Form2 : Form
+    {
+        //Contains dft buffer.
+        Complex[] cArray;
+        double[] dArray;
+        byte[] byteArray;
+        int[] bins;
+        int N = 50;
+        int filterSize;
+        int sampleRate;
+
+        // Form 2 (Takes In Complex Array, Length Of Buffer and Sample Rate)
+
+        int numThread = 6;
+        WaveHeader waveHeader;
+
+        public Form2(Complex[] inputArray, WaveHeader _waveHeader, double[] _dArray)
+        {
+
+
+            InitializeComponent();
+            ChartArea ca = dftChart.ChartAreas[0];  // quick reference
+
+            ca.CursorX.IsUserEnabled = true;
+            ca.CursorX.IsUserSelectionEnabled = false;
+            ca.CursorX.AutoScroll = false;
+            ca.AxisX.ScaleView.Zoomable = false;
+            lowfilter.Checked = true;
+            ca.CursorX.SelectionStart = 0;
+
+            waveHeader = _waveHeader;
+            sampleRate = waveHeader.sampleRate;
+            cArray = inputArray;
+            dArray = _dArray;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Display DFT Value On Graph
+        public void displayDFT()
+        {
+            dftChart.Series[0].Points.Clear();
+            for (int i = 0; i < cArray.Length; i++)
+            {
+                dftChart.Series[0].Points.Add(Math.Sqrt(Math.Pow(cArray[i].re, 2) + Math.Pow(cArray[i].im, 2)));
+            }
+        }
+
+        // Select Part Of the Graph Function
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            long selected = (long)dftChart.ChartAreas[0].CursorX.Position;
+            if (selected >= cArray.Length / 2)
+            {
+                selected = cArray.Length / 2;
+            }
+            dftChart.ChartAreas[0].CursorX.SelectionEnd = selected;
+        }
+
+        // Perform IDFT On Wave
+        public double[] IDFT(Complex[] A)
+        {
+            int len = A.Length;
+            double[] dArray = new double[len];
+
+            for (int t = 0; t < len; t++)
+            {
+                dArray[t] = 0;
+                for (int f = 0; f < len; f++)
+                {
+                    dArray[t] += A[f].re * Math.Cos(2 * Math.PI * f * t / len) - A[f].im * Math.Sin(2 * Math.PI * f * t / len);
+                }
+            }
+            return dArray;
+        }
+
+
+        // Filter 
+        private void filter_click(object sender, EventArgs e)
+
+        {
+            if (double.IsNaN(dftChart.ChartAreas[0].CursorX.SelectionEnd))
+            {
+                MessageBox.Show("Select values first");
+                return;
+            }
+            int[] filter;
+            double cutOffFreq = dftChart.ChartAreas[0].CursorX.Position;
+
+            int filterIndex = (int)Math.Ceiling(cutOffFreq * N / sampleRate);
+            if (lowfilter.Checked)
+            {
+                filter = createLowFilter(filterIndex);
+            }
+            else
+            {
+                filter = createHighFilter(filterIndex);
+            }
+            
+            double[] newFilter = IDFTfilter(filter);
+            convolution(newFilter);
+            
+
+        }
+
+        // Perform IDFT on Filtered Value
+        // Pre: filter as Integer Array
+        public double[] IDFTfilter(int[] filter) {
+            Complex[] complexFilter = new Complex[filter.Length];
+            for (int i = 0; i < filter.Length; i++)
+            {
+                complexFilter[i].re = filter[i];
+                complexFilter[i].im = 0;
+            }
+            return (IDFT(complexFilter));
+        }
+
+        // Check Filter Change
+        private void lowfilter_CheckedChanged(object sender, EventArgs e)
+        {
+            if(highfilter.Checked)
+            {
+                lowfilter.Checked = false;
+                dftChart.ChartAreas[0].CursorX.SelectionStart = cArray.Length / 2;
+
+            } else
+            {
+                highfilter.Checked = false;
+                dftChart.ChartAreas[0].CursorX.SelectionStart = 0;
+            }
+
+        }
+
+
+        // Perform Low Pass Filter
+        public int[] createLowFilter(int filterIndex)
+        {
+            int[] filter = new int[N];
+            int counter = 0;
+            for (; counter < filterIndex + 1; counter++)
+            {
+                filter[counter] = 1;
+            }
+            for (; counter < N - filterIndex; counter++)
+            {
+                filter[counter] = 0;
+            }
+            for (; counter < N; counter++)
+            {
+                filter[counter] = 1;
+            }
+            return filter;
+        }
+
+        // Perform High Pass Filter
+        public int[] createHighFilter(int filterIndex)
+        {
+            int[] filter = new int[N];
+            int counter = 0;
+            for (; counter < filterIndex + 1; counter++)
+            {
+                filter[counter] = 0;
+            }
+            for (; counter < N - filterIndex; counter++)
+            {
+                filter[counter] = 1;
+            }
+            for (; counter < N; counter++)
+            {
+                filter[counter] = 0;
+            }
+            return filter;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            N = (int)nVal.Value;
+        }
+
+        public void convolution(double[] filter)
+        {
+            double[] temp = new double[filter.Length];
+            double[] newDArray = new double[dArray.Length];
+            for (int i = 0; i< cArray.Length; i++)
+            {
+                for (int j = 0; j < filter.Length; j++)
+                {
+                    if (i + j <= dArray.Length)
+                    {
+                        temp[j] = filter[j] * dArray[i + j];
+
+                    }
+                }
+                newDArray[i] = sumArray(temp);
+            }
+            dArray = newDArray;
+            byteArray = convertDoubleToByteArray(dArray);
+            DFTthreading();
+            displayDFT();
+        }
+
+        public double sumArray(double[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+        static byte[] convertDoubleToByteArray(double[] dArray)
+        {
+            short[] sArray = new short[dArray.Length];
+            sArray = dArray.Select(x => (short)(x)).ToArray();
+            List<Byte> bList = new List<Byte>();
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                bList.AddRange(BitConverter.GetBytes(sArray[i]).ToList());
+            }
+            return bList.ToArray();
+        }
+
+        
+        public Complex[] DFTthreading()
+        {   
+            
+            
+            int dftLen = dArray.Length;
+            Complex[] cArray = new Complex[dftLen];
+            Thread[] t = new Thread[numThread];
+            Mutex mutex = new Mutex();
+
+            t[0] = new Thread(() => DFTthreadProc(cArray, 0, dftLen));
+            t[1] = new Thread(() => DFTthreadProc(cArray, 1, dftLen));
+            t[2] = new Thread(() => DFTthreadProc(cArray, 2, dftLen));
+            t[3] = new Thread(() => DFTthreadProc(cArray, 3, dftLen));
+            t[4] = new Thread(() => DFTthreadProc(cArray, 4, dftLen));
+            t[5] = new Thread(() => DFTthreadProc(cArray, 5, dftLen));
+            t[0].Start();
+            t[1].Start();
+            t[2].Start();
+            t[3].Start();
+            t[4].Start();
+            t[5].Start();
+            t[0].Join();
+            t[1].Join();
+            t[2].Join();
+            t[3].Join();
+            t[4].Join();
+            t[5].Join();
+            return cArray;
+        }
+
+
+        public  void DFTthreadProc(Complex[] cArray, int threadCount, int dftLen) {
+
+            int chunk = (dftLen / numThread);
+            Trace.WriteLine("Thread Proc count: "+ threadCount);
+            for (int f = threadCount * chunk; f < ((threadCount + 1) * chunk) - 1; f++)
+            { 
+
+                cArray[f].im = 0;
+                cArray[f].re = 0;
+                for (int t = 0; t < dftLen; t++)
+                {
+                    cArray[f].re += byteArray[t] * Math.Cos(2 * Math.PI * t * f / dftLen);
+                    cArray[f].im -= byteArray[t] * Math.Sin(2 * Math.PI * t * f / dftLen);
+
+                }
+                cArray[f].im /= dftLen;
+                cArray[f].re /= dftLen;
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            //Form1.setNewDoubleAndByteArray(dArray, byteArray);
+            this.Close();
+        }
+    }
+}
